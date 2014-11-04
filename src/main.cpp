e971e313096978aefb8f04452fd2c38a62a8a00f
devops001 <devops001@gmail.com>
2014-11-04 13:10:54 -0800

    added directories, main.cpp, and Rakefile

---
+++ src/main.cpp
@@ -0,0 +1,14 @@
+
+#include <iostream>
+#include <string>
+
+int main(int argc, char* argv[]) {
+
+  std::cout << "ok" << std::endl;
+
+
+  std::cout << "\npress Enter." << std::endl;
+  std::cin.get();
+  return 0;
+}
+
